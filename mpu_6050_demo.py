84703910bf72e308074d98ab53d44eddaa204f2c
Evan Zhang <redwolf0302@gmail.com>
2022-12-02 23:43:56 +0800

    Update mpu_6050_demo.py
    add buzzer for demo

--- mpu_6050_demo.py
+++ mpu_6050_demo.py
@@ -3,13 +3,26 @@ import sys
 from machine import SoftI2C, Pin
 from sensors.mpu_6050 import *
 from sensors.micropython_i2c import MICROPYTHON_I2C
-
+buzzer_pin = Pin(15, Pin.OUT)
 i2c = SoftI2C(scl=Pin(21), sda=Pin(20))
+
+
+def buzz(count, interval=50, duration=100):
+    assert count is not None and count > 0, "Buzz Error"
+    buzzer_pin.off()
+    for i in range(count):
+        buzzer_pin.on()
+        utime.sleep_ms(duration)
+        buzzer_pin.off()
+        utime.sleep_ms(interval)
+
+
 if __name__ == "__main__":
     sensor = MPU6050(driver=MICROPYTHON_I2C(MPU6050_DEFAULT_ADDRESS, i2c))
     sensor.initializeV2()
+    # sensor.initialize()
     print("initialize ok")
-#     sensor.initialize()
+    buzz(1)
 #     sensor.dmpInitialize()
     utime.sleep_ms(100)
     if not sensor.testConnection():
@@ -18,7 +31,10 @@ if __name__ == "__main__":
     else:
         print("connection ok")
 
+    count = 0
     while True:
+        if count % 100 == 0:
+            buzz(2, 50, 50)
         temp = sensor.getTemperature()/340+36.53
         (ax, ay, az, gx, gy, gz) = sensor.getMotion6()
         nax = ax*2.0 / 32768.0
@@ -27,9 +43,10 @@ if __name__ == "__main__":
         ngx = gx*250.0 / 32768.0
         ngy = gy*250.0 / 32768.0
         ngz = gz*250.0 / 32768.0
-#         print("ACCEL: x: %.2f, y: %.2f, z: %.2f, GYRO: x: %.2f, y: %.2f, z: %.2f" % (nax, nay, naz,ngx,ngy,ngz))
-#         print("ACCEL: x: %.2f, y: %.2f, z: %.2f" % (nax, nay, naz))
-#         print("GYRO: x: %.2f, y: %.2f, z: %.2f" % (ngx,ngy,ngz))
-        print("ACCEL: x: %.2f, y: %.2f, z: %.2f"%(nax, nay, naz))
-#         print("Temperature: %.2f" % (temp))
+        # print("ACCEL: x: %.2f, y: %.2f, z: %.2f, GYRO: x: %.2f, y: %.2f, z: %.2f" % (
+        #     nax, nay, naz, ngx, ngy, ngz))
+        print("ACCEL: x: %.2f, y: %.2f, z: %.2f" % (nax, nay, naz))
+        # print("GYRO: x: %.2f, y: %.2f, z: %.2f" % (ngx, ngy, ngz))
+        # print("Temperature: %.2f" % (temp))
         utime.sleep_ms(100)
+        count += 1
